647d6925af413987aeb44e815f74f556986895d3
DER25 <delizaga4@gmail.com>
2025-08-14 20:04:26 +0200

    tareas del ejercicio 1 hecjo

---
+++ ejercicio1.js
@@ -0,0 +1,19 @@
+// 1. Definir superhéroe favorito
+const miSuperheroeFavorito = 'Hulk'
+
+// 2. Definir número favorito
+const numeroFavorito = 50
+
+// 3. Definir dimensiones
+const altura = 5
+const longitud = 10
+
+// 4. Calcular la suma
+const suma = altura + longitud
+
+// 5. Mostrar resultados por consola
+console.log('Mi superhéroe favorito es:', miSuperheroeFavorito)
+console.log('Mi número favorito es:', numeroFavorito)
+console.log('Altura:', altura)
+console.log('Longitud:', longitud)
+console.log('La suma de altura y longitud es:', suma)
