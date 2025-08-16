340c2a27851636b78609eb9def9fa6472d97b879
DER25 <delizaga4@gmail.com>
2025-08-16 14:00:20 +0200

    ejercicio 4 y 5

--- ejercicio3.js
+++ ejercicio3.js
@@ -5,3 +5,14 @@ console.log(10 / 2) // Resultado: 5
 
 // 3.3 Muestra el resto de dividir 15 por 9
 console.log(15 % 9) // Resultado: 6
+// 3.4 Usa el operador de asignación para obtener o = 15
+let p = 10
+let j = 5
+let o = p + j
+console.log(o) // Resultado: 15
+
+// 3.5 Usa el operador de asignación para obtener i = 50
+let c = 10
+let m = 5
+let i = c * m
+console.log(i) // Resultado: 50
