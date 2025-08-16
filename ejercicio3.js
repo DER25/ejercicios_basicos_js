90f0444fa3d808a040f9ddbd06b2b8c873274cc0
DER25 <delizaga4@gmail.com>
2025-08-16 13:59:54 +0200

    ejercicio 2 y 3

--- ejercicio3.js
+++ ejercicio3.js
@@ -1,2 +1,7 @@
 // 3.1 Multiplica 10 por 5
 console.log(10 * 5) // Resultado: 50
+// 3.2 Divide 10 por 2
+console.log(10 / 2) // Resultado: 5
+
+// 3.3 Muestra el resto de dividir 15 por 9
+console.log(15 % 9) // Resultado: 6
